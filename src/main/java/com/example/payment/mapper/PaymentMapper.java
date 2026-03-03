e53faceb1bfeb3498a0225b490e51cb44bd18b28
rame86 <ssrabw86@gmail.com>
2026-03-03 15:13:35 +0900

    db조회 업데이트

--- src/main/java/com/example/payment/mapper/PaymentMapper.java
+++ src/main/java/com/example/payment/mapper/PaymentMapper.java
@@ -2,14 +2,21 @@
 
 package com.example.payment.mapper;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.payment.dto.response.WalletResponseDTO;
 
 @Mapper
 public interface PaymentMapper {
+
     // 모든 지갑 정보를 조회하는 메서드
     List<WalletResponseDTO> findAllWallets();
+
+    // member_id로 잔액 조회 
+    BigDecimal getBalanceByMemberId(@Param("memberId") Long memberId);
+    
 }
