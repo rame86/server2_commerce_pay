ebf55fc85188a57fd2cc1977d84a5e22c5bce0b1
rame86 <ssrabw86@gmail.com>
2026-03-06 13:07:25 +0900

    이벤트 DTO 최적화

--- src/main/java/com/example/payment/dto/event/PaymentEventDTO.java
+++ src/main/java/com/example/payment/dto/event/PaymentEventDTO.java
@@ -14,20 +14,16 @@ import lombok.ToString;
 @ToString // 수신 확인용 로그를 찍을 때 객체 안의 값을 보기 위해 추가.
 public class PaymentEventDTO {
 
-    // Node.js에서 보내는 JSON의 Key 값과 변수명이 정확히 일치해야 자동 매핑.
-    private String orderId; // res.reservations의 reservation_id (결제 원장의 reference_id로 사용)
+    // JSON의 Key 값과 변수명이 정확히 일치해야 자동 매핑.
+    private String orderId; // 주문아이디
     private Long memberId; // 결제자 member_id (지갑 조회를 위해 필수)
-    private BigDecimal amount; // 총 결제 금액 (total_price)
-    private String type; // 요청 타입
-    private String eventTitle; // 결제 상세 내역에 기록할 공연명 혹은 상품명
+    private BigDecimal amount; // 총 결제 혹은 환불 요청 금액
+    private String type; // 요청 타입 (결제 or 환불 or 후원)
+    private String eventTitle; // 결제 상세 내역에 기록 내용(공연명 or 상품명 or 후원자명)
     private String replyRoutingKey; // 요청한 서비스가 응답받길 원하는 라우팅 키
 
     public String getReservationId() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
         
-    private String payType; // "kakao_pay", "naver_pay", "bank_transfer" 등
-
- 
-
 }
